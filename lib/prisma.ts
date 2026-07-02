4fe3984e1180640d2de21a73263ade2ed45047bf
ChMubasharAli <muhammadmubashirali075@gmail.com>
2026-07-02 11:26:01 +0500

    update changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,25 @@
-import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client";
 
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
+};
+
 const adapter = new PrismaMariaDb({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USERNAME!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_DATABASE!,
   port: Number(process.env.DB_PORT),
   ssl: { rejectUnauthorized: true },
 });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
